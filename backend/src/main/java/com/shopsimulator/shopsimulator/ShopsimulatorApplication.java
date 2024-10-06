6f9e131abaa92ab6a0b298b1df736b8481e4ef62
Andrew <andrew.segovia45@gmail>
2024-10-05 22:02:26 -0700

    springboot template added to backend

---
+++ backend/src/main/java/com/shopsimulator/shopsimulator/ShopsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.shopsimulator.shopsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsimulatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shopsimulator/shopsimulator/ShopsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsimulator.shopsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
