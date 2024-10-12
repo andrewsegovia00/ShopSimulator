4607843ae84be6f82bf53c76c08bb9bfd1bc4a2c
Andrew <andrew.segovia45@gmail>
2024-10-11 21:56:42 -0700

    Addition of the Bonus class, update of the Employee class to accept liked, and disliked Employees

--- backend/src/main/java/com/shopsimulator/shopsimulator/entity/Employee.java
+++ backend/src/main/java/com/shopsimulator/shopsimulator/entity/Employee.java
@@ -1,24 +1,27 @@
-package com.shopsimulator.shopsimulator.entity;
+package com.backend.springbootfirebase.entity;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Employee {
 
     private String name;
     private int id;
+    private String description;
+    //    List<empBonus> empBonus;
+//    List<empBonus> empClash;
+    private List<String> bonusIds;
+    private List<String> likedEmployeeIds;
+    private List<String> dislikedEmployeeIds;
 
-    public int getBonus() {
-        return bonus;
-    }
+    public Employee(String name, int empId, String empDescription) {
+        this.name = name;
+        this.id = empId;
+        this.description = empDescription;
+        this.bonusIds = new ArrayList<>();
+        this.likedEmployeeIds = new ArrayList<>();
+        this.dislikedEmployeeIds = new ArrayList<>();
 
-    public void setBonus(int bonus) {
-        this.bonus = bonus;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -29,6 +32,44 @@ public class Employee {
         this.name = name;
     }
 
-    private int bonus;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getBonusIds() {
+        return bonusIds;
+    }
+
+    public void setBonusIds(List<String> bonusIds) {
+        this.bonusIds = bonusIds;
+    }
+
+    public List<String> getLikedEmployeeIds() {
+        return likedEmployeeIds;
+    }
+
+    public void setLikedEmployeeIds(List<String> likedEmployeeIds) {
+        this.likedEmployeeIds = likedEmployeeIds;
+    }
+
+    public List<String> getDislikedEmployeeIds() {
+        return dislikedEmployeeIds;
+    }
+
+    public void setDislikedEmployeeIds(List<String> dislikedEmployeeIds) {
+        this.dislikedEmployeeIds = dislikedEmployeeIds;
+    }
 
 }
